6730abdcda678585a89be79c6a12ecfc38276054
arisukwu-joan <joanarisukwu@gmail.com>
2022-06-16 02:22:41 +0100

    changes on humburger initial commit

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const toggle_btn = document.querySelector('.dropdown-toggle');
 const menu = document.querySelector('.header .menu')
 
 toggle_btn.addEventListener('click', ()=>{
-    if(menu.classList.contains('active')){
-        menu.classList.remove('active')
-    } else{
+    if(!(menu.classList.contains('active'))){
         menu.classList.add('active')
+        menu.style.display = "block";
+    } else{
+        menu.classList.remove('active')
+        menu.style.display = "none"; 
     }
-})
+}
+)
+
